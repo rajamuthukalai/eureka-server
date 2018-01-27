43ae993c8a63454bf42d69294fb33b4b218c8e8c
Raja <raja.muthukalai@yahoo.com>
2018-01-27 19:21:47 +0530

    eureka server base code

---
+++ src/main/java/com/sannu/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,29 @@
+package com.sannu.eurekaserver;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
+
+@RestController
+class DefaultController {
+	
+	@Value("${message:default message}")
+	private String message;
+	
+	@GetMapping("/message")
+	public String message() {
+		return message;
+	}
+}
